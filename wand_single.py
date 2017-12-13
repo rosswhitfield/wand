aa0f859aaf2193d629516eb1c869aefd2497c666
Ross Whitfield <whitfieldre@ornl.gov>
2017-12-13 12:19:24 -0500

    cron update

--- wand_single.py
+++ wand_single.py
@@ -1,5 +1,4 @@
 from mantid.simpleapi import *
-from mantid.api import NumericAxis
 
 van = LoadEventNexus(Filename='/HFIR/HB2C/IPTS-7776/nexus/HB2C_1035.nxs.h5')
 van = Integration(van)
@@ -15,3 +14,23 @@ w = np.array([1.487,1.489])
 for idx in xrange(ws.getNumberHistograms()):
     ws.setX(idx, w)
 ws=ConvertToEventWorkspace(ws)
+SetGoniometer(ws, Axis0="HB2C:Mot:s1,0,1,0,1")
+ConvertToMD(ws, QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', OutputWorkspace='md')
+
+
+# Do multiple
+
+if 'md' in mtd:
+    mtd.remove('md')
+
+for run in range(1059,1833,100):
+    ws = LoadEventNexus(Filename='/HFIR/HB2C/IPTS-7776/nexus/HB2C_{}.nxs.h5'.format(run))
+    ws = Integration(ws)
+    MaskDetectors(ws,DetectorList=range(16384))
+    ws.getAxis(0).setUnit("Wavelength")
+    w = np.array([1.487,1.489])
+    for idx in xrange(ws.getNumberHistograms()):
+        ws.setX(idx, w)
+    ws=ConvertToEventWorkspace(ws)
+    SetGoniometer(ws, Axis0="HB2C:Mot:s1,0,1,0,1")
+    ConvertToMD(ws, QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', OutputWorkspace='md',OverwriteExisting=False,MinValues='-10,-10,-10',MaxValues='10,10,10')
