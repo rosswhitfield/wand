9e3d715cff5ecaeb90a89ce67e21748e4016972e
Ross Whitfield <whitfieldre@ornl.gov>
2017-12-13 14:19:21 -0500

    cron update

--- wand_single.py
+++ wand_single.py
@@ -9,6 +9,7 @@ w = np.array([1.487,1.489])
 for idx in xrange(van.getNumberHistograms()):
     van.setX(idx, w)
 van=ConvertToEventWorkspace(van)
+SetGoniometer('van', Axis0="HB2C:Mot:s1,0,1,0,1")
 
 """
 ws = LoadEventNexus(Filename='/HFIR/HB2C/IPTS-7776/nexus/HB2C_1078.nxs.h5')
@@ -47,15 +48,26 @@ for run in range(1059,1834,1):
     ConvertToMD('ws', QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', OutputWorkspace='md',OverwriteExisting=False,MinValues='-10,-10,-10',MaxValues='10,10,10')
     # Van, copy goniometer
     mtd['van'].run().getGoniometer().setR(mtd['ws'].run().getGoniometer().getR())
-    ConvertToMD('ws', QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', OutputWorkspace='van_md',OverwriteExisting=False,MinValues='-10,-10,-10',MaxValues='10,10,10')
+    ConvertToMD('van', QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', OutputWorkspace='van_md',OverwriteExisting=False,MinValues='-10,-10,-10',MaxValues='10,10,10')
     if run%20 == 0:
         SaveMD('md', '/SNS/users/rwp/wand/Ho2PdSi3_data_MDE.nxs')
         SaveMD('van_md', '/SNS/users/rwp/wand/Ho2PdSi3_van_MDE.nxs')
+        mdh = BinMD(InputWorkspace='md', AlignedDim0='Q_sample_x,-10,10,401', AlignedDim1='Q_sample_y,-1,1,41', AlignedDim2='Q_sample_z,-10,10,401')
+        van_mdh = BinMD(InputWorkspace='van_md', AlignedDim0='Q_sample_x,-10,10,401', AlignedDim1='Q_sample_y,-1,1,41', AlignedDim2='Q_sample_z,-10,10,401')
+        norm = mdh/van_mdh
+        SaveMD('mdh', '/SNS/users/rwp/wand/Ho2PdSi3_data_MDH.nxs')
+        SaveMD('van_mdh', '/SNS/users/rwp/wand/Ho2PdSi3_van_MDH.nxs')
+        SaveMD('norm', '/SNS/users/rwp/wand/Ho2PdSi3_norm_MDH.nxs')
 
 SaveMD('md', '/SNS/users/rwp/wand/Ho2PdSi3_data_MDE.nxs')
 SaveMD('van_md', '/SNS/users/rwp/wand/Ho2PdSi3_van_MDE.nxs')
+SaveMD('mdh', '/SNS/users/rwp/wand/Ho2PdSi3_data_MDH.nxs')
+SaveMD('van_mdh', '/SNS/users/rwp/wand/Ho2PdSi3_van_MDH.nxs')
+SaveMD('norm', '/SNS/users/rwp/wand/Ho2PdSi3_norm_MDH.nxs')
 
 
-# md=LoadMD('/SNS/users/rwp/wand/Ho2PdSi3_data_MDE.nxs')
-# van_md=LoadMD('/SNS/users/rwp/wand/Ho2PdSi3_van_MDE.nxs')
-
+# md=LoadMD('/SNS/users/rwp/wand/Ho2PdSi3_data_MDE.nxs', LoadHistory=False)
+# van_md=LoadMD('/SNS/users/rwp/wand/Ho2PdSi3_van_MDE.nxs', LoadHistory=False)
+# mdh=LoadMD('/SNS/users/rwp/wand/Ho2PdSi3_data_MDH.nxs', LoadHistory=False)
+# van_mdh=LoadMD('/SNS/users/rwp/wand/Ho2PdSi3_van_MDH.nxs', LoadHistory=False)
+# norm=LoadMD('/SNS/users/rwp/wand/Ho2PdSi3_norm_MDH.nxs', LoadHistory=False)
