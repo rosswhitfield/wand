52e61b7b64e0af839e4179184b3a414bfe43c48a
Ross Whitfield <whitfieldre@ornl.gov>
2017-12-14 12:19:13 -0500

    cron update

--- Ho2PdSi3_merge_MD.py
+++ Ho2PdSi3_merge_MD.py
@@ -2,7 +2,12 @@ from mantid.simpleapi import *
 
 start = 1059
 end = 1833
-end = 1063
+end = 1450
+
+if 'mdh' in mtd:
+    mtd.remove('mdh')
+if 'van_mdh' in mtd:
+    mtd.remove('van_mdh')
 
 for run in range(start,end+1,1):
     if run == 1796:
@@ -22,6 +27,6 @@ for run in range(start,end+1,1):
         van_mdh=CloneWorkspace(van)
 
 norm = mdh/van_mdh
-#SaveMD('mdh', '/SNS/users/rwp/wand/Ho2PdSi3_data_MDH_{}_{}.nxs'.format(start, end))
-#SaveMD('van_mdh', '/SNS/users/rwp/wand/Ho2PdSi3_van_MDH_{}_{}.nxs'.format(start, end))
-#SaveMD('norm', '/SNS/users/rwp/wand/Ho2PdSi3_norm_MDH_{}_{}.nxs'.format(start, end))
+SaveMD('mdh', '/SNS/users/rwp/wand/Ho2PdSi3_data_MDH_{}_{}.nxs'.format(start, end))
+SaveMD('van_mdh', '/SNS/users/rwp/wand/Ho2PdSi3_van_MDH_{}_{}.nxs'.format(start, end))
+SaveMD('norm', '/SNS/users/rwp/wand/Ho2PdSi3_norm_MDH_{}_{}.nxs'.format(start, end))
