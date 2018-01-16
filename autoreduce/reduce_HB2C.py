0b02a171fe3b005eeb40a6fc482fa6c64082cc05
Ross Whitfield <whitfieldre@ornl.gov>
2018-01-16 17:19:10 -0500

    cron update

---
+++ autoreduce/reduce_HB2C.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2
+import sys
+import numpy as np
+
+sys.path.append("/opt/mantidnightly/bin")
+from mantid.simpleapi import *
+from mantid import logger
+
+if (len(sys.argv) != 3): 
+    logger.error("autoreduction code requires a filename and an output directory")
+    sys.exit()
+if not(os.path.isfile(sys.argv[1])):
+    logger.error("data file "+sys.argv[1]+ " not found")
+    sys.exit()    
+else:
+    filename = sys.argv[1]
+    outdir = sys.argv[2]
+nexus_file=sys.argv[1]
+output_directory=sys.argv[2]
+output_file=os.path.split(nexus_file)[-1].replace('.nxs.h5','')
+ipts = nexus_file.split('/')[3]
+
+ws= LoadEventNexus(Filename=filename)
+ws = Integration(ws)
+MaskDetectors(ws,DetectorList=range(16384))
+ws.getAxis(0).setUnit("Wavelength")
+w = np.array([1.487,1.489])
+for idx in xrange(ws.getNumberHistograms()):
+    ws.setX(idx, w)
+SetGoniometer('ws', Axis0="HB2C:Mot:s1,0,1,0,1")
+SaveNexus('ws',os.path.join(output_directory,output_file+".nxs"))
--- single2/NaCl.py
+++ single2/NaCl.py
@@ -45,15 +45,6 @@ for run in range(2952,4755,100):
     # Van, copy goniometer
     mtd['van'].run().getGoniometer().setR(mtd['ws'].run().getGoniometer().getR())
     ConvertToMD('van', QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', OutputWorkspace='van_md',OverwriteExisting=False,MinValues='-10,-10,-10',MaxValues='10,10,10')
-    if run%20 == 0:
-        SaveMD('md', '/SNS/users/rwp/wand/NaCl_data_MDE.nxs')
-        SaveMD('van_md', '/SNS/users/rwp/wand/NaCl_van_MDE.nxs')
-        mdh = BinMD(InputWorkspace='md', AlignedDim0='Q_sample_x,-10,10,401', AlignedDim1='Q_sample_y,-1,1,41', AlignedDim2='Q_sample_z,-10,10,401')
-        van_mdh = BinMD(InputWorkspace='van_md', AlignedDim0='Q_sample_x,-10,10,401', AlignedDim1='Q_sample_y,-1,1,41', AlignedDim2='Q_sample_z,-10,10,401')
-        norm = mdh/van_mdh
-        SaveMD('mdh', '/SNS/users/rwp/wand/NaCl_data_MDH.nxs')
-        SaveMD('van_mdh', '/SNS/users/rwp/wand/NaCl_van_MDH.nxs')
-        SaveMD('norm', '/SNS/users/rwp/wand/NaCl_norm_MDH.nxs')
 
 SaveMD('md', '/SNS/users/rwp/wand/NaCl_data_MDE.nxs')
 SaveMD('van_md', '/SNS/users/rwp/wand/NaCl_van_MDE.nxs')
