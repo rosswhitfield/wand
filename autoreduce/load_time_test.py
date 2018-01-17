4acc191252c68bd9bdf2387ebfea749691c14322
Ross Whitfield <whitfieldre@ornl.gov>
2018-01-17 15:19:11 -0500

    cron update

--- autoreduce/load_time_test.py
+++ autoreduce/load_time_test.py
@@ -1,4 +1,5 @@
-from mantid.simpleapi import *
+from mantid.simpleapi import LoadNexus
 
 for run in range(6530,6534):
+    print('HB2C_{}.nxs'.format(run))
     ws = LoadNexus('/HFIR/HB2C/IPTS-7776/shared/autoreduce/HB2C_{}.nxs'.format(run))
