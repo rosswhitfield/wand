ef24958e6d1a3ac4177a1764a1c7bcd2aa4800d9
Ross Whitfield <whitfieldre@ornl.gov>
2018-01-17 12:19:12 -0500

    cron update

--- single2/create_grouping_file2.py
+++ single2/create_grouping_file2.py
@@ -12,6 +12,6 @@ for x in range(0,480*8,4):
         group += 1
         for j in range(4):
             for i in range(4):
-                grouping.dataY(i)[0] = group
+                grouping.dataY(y+i+(x+j)*512)[0] = group
 
 SaveDetectorsGrouping(grouping, "HB2C_4x4.xml")
