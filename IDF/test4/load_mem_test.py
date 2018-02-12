5a2b63e7f20da26074582e63bf13f01c64334fb2
Ross Whitfield <whitfieldre@ornl.gov>
2018-02-11 19:19:22 -0500

    cron update

--- IDF/test4/load_mem_test.py
+++ IDF/test4/load_mem_test.py
@@ -20,28 +20,34 @@ print(m3-m2)
 print(m4-m3)
 print(m5-m4)
 
-for n in range(10):
+for n in range(5):
     m10=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-    LoadEmptyInstrument(Filename='/SNS/users/rwp/wand/IDF/test4/WAND_Definition_fixed.xml', OutputWorkspace='wand {}'.format(n))
+    LoadEmptyInstrument(Filename='/SNS/users/rwp/wand/IDF/test4/WAND_Definition_2952.xml', OutputWorkspace='wand{}'.format(n+300))
     m13=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-    print(m13-m10)
+    print("rect",m13-m10)
 
-
-for n in range(10):
+for n in range(5):
     m10=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-    LoadEmptyInstrument(Filename='/SNS/users/rwp/wand/IDF/test4/WAND_Definition.xml', OutputWorkspace='wand {}'.format(n))
+    LoadEmptyInstrument(Filename='/SNS/users/rwp/wand/IDF/test4/WAND_Definition_fixed.xml', OutputWorkspace='wand{}'.format(n+100))
     m13=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-    print(m13-m10)
+    print("fixed",m13-m10)
 
 
-for n in range(10):
+for n in range(5):
     m10=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-    LoadEmptyInstrument(Filename='/SNS/users/rwp/wand/IDF/test4/WAND_Definition.xml', OutputWorkspace='wand {}'.format(n))
+    LoadEmptyInstrument(Filename='/SNS/users/rwp/wand/IDF/test4/WAND_Definition.xml', OutputWorkspace='wand{}'.format(n+200))
+    m13=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+    print("wand",m13-m10)
+
+
+for n in range(5):
+    m10=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+    LoadEmptyInstrument(Filename='/SNS/users/rwp/wand/IDF/test4/WAND_Definition.xml', OutputWorkspace='wand{}'.format(n))
     m11=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-    AddSampleLog('wand {}'.format(n), LogName='HB2C:Mot:s2.RBV', LogText='17.57', LogType='Number Series')
-    AddSampleLog('wand {}'.format(n), LogName='HB2C:Mot:detz.RBV', LogText='7.05159', LogType='Number Series')
+    AddSampleLog('wand{}'.format(n), LogName='HB2C:Mot:s2.RBV', LogText='17.57', LogType='Number Series')
+    AddSampleLog('wand{}'.format(n), LogName='HB2C:Mot:detz.RBV', LogText='7.05159', LogType='Number Series')
     m12=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-    LoadInstrument('wand {}'.format(n),Filename='/SNS/users/rwp/wand/IDF/test4/WAND_Definition.xml', RewriteSpectraMap=False)
+    LoadInstrument('wand{}'.format(n),Filename='/SNS/users/rwp/wand/IDF/test4/WAND_Definition.xml', RewriteSpectraMap=False)
     m13=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
     print(m11-m10)
     print(m12-m11)
