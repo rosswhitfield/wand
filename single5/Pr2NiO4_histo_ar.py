093232c1becdd18a6242e72c0cb8d4d1dee9dffa
Ross Whitfield <whitfieldre@ornl.gov>
2018-02-14 13:19:12 -0500

    cron update

--- single5/Pr2NiO4_histo_ar.py
+++ single5/Pr2NiO4_histo_ar.py
@@ -4,7 +4,10 @@ if 'data' in mtd:
     mtd.remove('data')
     
 for run in range(4756,6557,1):
-    LoadMD(Filename='/HFIR/HB2C/IPTS-7776/shared/autoreduce/HB2C_{}_MDE.nxs'.format(run),OutputWorkspace='md')
+    try:
+        LoadMD(Filename='/HFIR/HB2C/IPTS-7776/shared/autoreduce/HB2C_{}_MDE.nxs'.format(run),OutputWorkspace='md')
+    except:
+        continue
     BinMD(InputWorkspace='md', OutputWorkspace='mdh', AlignedDim0='Q_sample_x,-10,10,401', AlignedDim1='Q_sample_y,-1,1,41', AlignedDim2='Q_sample_z,-10,10,401')
     if 'data' in mtd:
         PlusMD(LHSWorkspace='data', RHSWorkspace='mdh', OutputWorkspace='data')
