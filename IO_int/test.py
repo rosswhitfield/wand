e6261088981111c9d892a5602a956187c99191f0
Ross Whitfield <whitfieldre@ornl.gov>
2018-02-15 16:19:08 -0500

    cron update

--- IO_int/test.py
+++ IO_int/test.py
@@ -3,10 +3,11 @@ import numpy as np
 
 van=LoadNexus('/HFIR/HB2C/IPTS-7776/shared/autoreduce/HB2C_{}.nxs'.format(6558))
 
-a=van.extractY()
-np.save('out.npy',a)
-a2=np.load('out.npy')
-new=np.random.random(a.shape)
+y=van.extractY()
+e=van.extractY()
+x=np.full(y.shape, 1.488)
+w=1.488
 
-for n, val in enumerate(new):
-    van.setY(n,val)
+CreateWorkspace(OutputWorkspace='new', DataX=1.488, DataY=y, DataE=e, NSpec=y.size, UnitX='Wavelength')
+
+LoadInstrument('new', InstrumentName='WAND')
