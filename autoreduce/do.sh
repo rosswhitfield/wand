9d1f783946ed25cc63cbf86a3d4e250f9aac3f02
Ross Whitfield <whitfieldre@ornl.gov>
2018-02-14 22:19:14 -0500

    cron update

--- autoreduce/do.sh
+++ autoreduce/do.sh
@@ -3,6 +3,6 @@
 for i in `seq 4756 5000`;
 do
     echo $i
-    mantidpythonnightly /HFIR/HB2C/shared/autoreduce/reduce_HB2C.py /HFIR/HB2C/IPTS-7776/nexus/HB2C_$i.nxs.h5 /HFIR/HB2C/IPTS-7776/shared/autoreduce/
+    mantidpythonnightly /HFIR/HB2C/shared/autoreduce/reduce_HB2C.py /HFIR/HB2C/IPTS-7776/nexus/HB2C_$i.nxs.h5 /HFIR/HB2C/IPTS-7776/shared/rwp/PNO/
 done
 
