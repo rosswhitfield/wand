9d0dd13a3feb7cacd498920364690788b9468985
Ross Whitfield <whitfieldre@ornl.gov>
2018-02-16 18:19:11 -0500

    cron update

---
+++ single5/Pr2NiO4_event_ar3.py
@@ -0,0 +1,19 @@
+from mantid.simpleapi import *
+
+if 'dataE' in mtd:
+    mtd.remove('dataE')
+    
+for run in range(4756,6557,5):
+    LoadMD(Filename='/HFIR/HB2C/IPTS-7776/shared/autoreduce/HB2C_{}_MDE.nxs'.format(run),OutputWorkspace='md')
+    if 'dataE' in mtd:
+        PlusMD(LHSWorkspace='dataE', RHSWorkspace='md', OutputWorkspace='dataE')
+    else:
+        CloneMDWorkspace(InputWorkspace='md', OutputWorkspace='dataE')
+
+
+FindPeaksMD(InputWorkspace='dataE', PeakDistanceThreshold=0.2, DensityThresholdFactor=500, CalculateGoniometerForCW=True, Wavelength=1.488, OutputWorkspace='peaks')
+FindUBUsingFFT('peaks',MinD=3,MaxD=15)
+ShowPossibleCells('peaks')
+SelectCellWithForm('peaks',FormNumber=42,Apply=True)
+OptimizeLatticeForCellType('peaks', CellType='Orthorhombic',Apply=True)
+SaveIsawUB('peaks',Filename='/SNS/users/rwp/PNO.mat')
--- single5/Pr2NiO4_histo_ar3.py
+++ single5/Pr2NiO4_histo_ar3.py
@@ -3,10 +3,18 @@ from mantid.simpleapi import *
 if 'data' in mtd:
     mtd.remove('data')
     
-for run in range(4756,6557,1):
+for run in range(4756,6557,5):
     LoadMD(Filename='/HFIR/HB2C/IPTS-7776/shared/autoreduce/HB2C_{}_MDE.nxs'.format(run),OutputWorkspace='md')
     BinMD(InputWorkspace='md', OutputWorkspace='mdh', AlignedDim0='Q_sample_x,-10,10,401', AlignedDim1='Q_sample_y,-1,1,41', AlignedDim2='Q_sample_z,-10,10,401')
     if 'data' in mtd:
         PlusMD(LHSWorkspace='data', RHSWorkspace='mdh', OutputWorkspace='data')
     else:
         CloneMDWorkspace(InputWorkspace='mdh', OutputWorkspace='data')
+
+
+FindPeaksMD(InputWorkspace='data', PeakDistanceThreshold=0.2, DensityThresholdFactor=500, CalculateGoniometerForCW=True, Wavelength=1.488, OutputWorkspace='peaks')
+FindUBUsingFFT('peaks',MinD=3,MaxD=15)
+ShowPossibleCells('peaks')
+SelectCellWithForm('peaks',FormNumber=42,Apply=True)
+OptimizeLatticeForCellType('peaks', CellType='Orthorhombic',Apply=True)
+SaveIsawUB('peaks',Filename='/SNS/users/rwp/PNO.mat')
