3c075df787b3ccf58759a826df8b4c8ee5598b03
Ross Whitfield <whitfieldre@ornl.gov>
2018-02-20 12:19:12 -0500

    cron update

--- Pr2NiO4.py
+++ Pr2NiO4.py
@@ -7,7 +7,7 @@ van = LoadNexus('/HFIR/HB2C/IPTS-7776/shared/autoreduce/HB2C_{}.nxs'.format(6558
 if 'data' in mtd:
     DeleteWorkspace('data')
 
-for filename in ['/HFIR/HB2C/IPTS-7776/shared/rwp/PNOe/HB2C_{}_MDE.nxs'.format(run) for run in range(4756,6557,5)]:
+for filename in ['/HFIR/HB2C/IPTS-7776/shared/rwp/PNOe/HB2C_{}_MDE.nxs'.format(run) for run in range(4756,6557,4)]:
                  LoadMD(Filename=filename, LoadHistory=False, OutputWorkspace='md')
                  accumulateMD('md', OutputWorkspace='data')
 
