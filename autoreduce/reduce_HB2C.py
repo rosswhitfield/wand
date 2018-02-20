5cf1f62de1d0f2ef26cabea92421d601802796f3
Ross Whitfield <whitfieldre@ornl.gov>
2018-02-20 14:19:14 -0500

    cron update

--- autoreduce/reduce_HB2C.py
+++ autoreduce/reduce_HB2C.py
@@ -20,7 +20,7 @@ output_directory=sys.argv[2]
 output_file=os.path.split(nexus_file)[-1].replace('.nxs.h5','')
 ipts = nexus_file.split('/')[3]
 
-ws = LoadEventNexus(Filename=filename)
+ws, mon = LoadEventNexus(Filename=filename, LoadMonitors=True)
 ws = Integration(ws)
 MaskDetectors(ws,DetectorList=range(16384))
 ws.getAxis(0).setUnit("Wavelength")
@@ -28,8 +28,10 @@ w = np.array([1.487,1.489])
 for idx in xrange(ws.getNumberHistograms()):
     ws.setX(idx, w)
 SetGoniometer('ws', Axis0="HB2C:Mot:s1,0,1,0,1")
-#SaveNexus('ws',os.path.join(output_directory,output_file+".nxs"))
-ConvertToMD('ws', QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', OutputWorkspace='md')
+AddSampleLog(ws,LogName="gd_prtn_chrg",LogType='Number',NumberType='Double',
+             LogText=str(mon.getNumberEvents()))
+SaveNexus('ws',os.path.join(output_directory,output_file+".nxs"))
+ConvertToMD('ws', QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', OutputWorkspace='md',MinValues='-10,-1,-10',MaxValues='10,1,10')
 SaveMD('md',os.path.join(output_directory,output_file+"_MDE.nxs"))
 
 # Group data
