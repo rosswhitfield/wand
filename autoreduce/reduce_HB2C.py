8acdff9bc9d5e54083866ab1bdeb29855184bf62
Ross Whitfield <whitfieldre@ornl.gov>
2018-03-13 15:19:14 -0400

    Update SCDNorm.py autoreduce/reduce_HB2C.py

--- autoreduce/reduce_HB2C.py
+++ autoreduce/reduce_HB2C.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-sys.path.append("/opt/mantidnightly/bin") # noqa
+sys.path.append("/opt/mantidnightly/bin")
 from mantid.simpleapi import *
 from mantid import logger
 
@@ -26,15 +26,29 @@ ConvertToMD('ws', QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_samp
 SaveMD('md',os.path.join(output_directory,output_file+"_MDE.nxs"))
 
 if powder:
-    #SaveNexus('ws',os.path.join(output_directory,output_file+".nxs"))
+    SaveNexus('ws',os.path.join(output_directory,output_file+".nxs"))
     runNumber = mtd['ws'].getRunNumber()
+    s2 = mtd['ws'].run().getLogData('HB2C:Mot:s2').firstValue()
     ConvertSpectrumAxis(InputWorkspace='ws', Target='Theta', OutputWorkspace='ws')
     Transpose(InputWorkspace='ws', OutputWorkspace='ws')
-    ResampleX(InputWorkspace='ws', OutputWorkspace='ws', XMin=1, XMax=179, NumberBins=3561)
+    ResampleX(InputWorkspace='ws', OutputWorkspace='ws', XMin=s2, XMax=s2+120, NumberBins=2400)
+
+    if True: # do cal and monitor normalisation
+        van_file = '/HFIR/HB2C/IPTS-7776/shared/autoreduce/HB2C_7553.nxs'
+        cal = LoadNexus(Filename=van_file)
+        ws_monitor = mtd['ws'].run().getProtonCharge()
+        cal_monitor = cal.run().getProtonCharge()
+        CopyInstrumentParameters('ws', 'cal')
+        ConvertSpectrumAxis(InputWorkspace='cal', Target='Theta', OutputWorkspace='cal')
+        Transpose(InputWorkspace='cal', OutputWorkspace='cal')
+        ResampleX(InputWorkspace='cal', OutputWorkspace='cal', XMin=s2, XMax=s2+120, NumberBins=2400)
+        Divide(LHSWorkspace='ws', RHSWorkspace='cal', OutputWorkspace='ws')
+        Scale(InputWorkspace='ws', OutputWorkspace='ws', Factor=cal_monitor/ws_monitor)
+
     div = SavePlot1D('ws', OutputType='plotly')
-    from finddata import publish_plot
+    from postprocessing.publish_plot import publish_plot
+    #from finddata import publish_plot
     request = publish_plot('HB2C', runNumber, files={'file': div})
     print("post returned %d" % request.status_code)
     print("resulting document:")
     print(request.text)
-
