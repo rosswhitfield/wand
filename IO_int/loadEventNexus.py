1cf670be15e77c379e23fe7db775915838ce5d8d
Ross Whitfield <whitfieldre@ornl.gov>
2018-04-02 15:19:14 -0400

    Update IO_int/loadEventNexus.py

--- IO_int/loadEventNexus.py
+++ IO_int/loadEventNexus.py
@@ -1,5 +1,6 @@
 import h5py
 import numpy as np
+import time
 run=6558
 filename='/HFIR/HB2C/IPTS-7776/nexus/HB2C_{}.nxs.h5'.format(run)
 
@@ -11,17 +12,10 @@ wavelenght = 1.488
 
 pixels = 480*512*8
 
-values = np.empty((0),dtype=np.int64)
-histo = np.zeros((pixels),dtype=np.int64)
+bc2 = np.empty((pixels),dtype=np.int64)
 with h5py.File(filename, 'r') as f:
     for b in range(8):
-        values = np.concatenate((values,f['/entry/bank'+str(b+1)+'_events/event_id'].value))
+        bc2 += np.bincount(f['/entry/bank'+str(b+1)+'_events/event_id'].value,minlength=pixels)
     s1 = f['entry/DASlogs/HB2C:Mot:s1.RBV/average_value'].value[0]
     s2 = f['entry/DASlogs/HB2C:Mot:s2.RBV/average_value'].value[0]
     detz = f['entry/DASlogs/HB2C:Mot:detz.RBV/average_value'].value[0]
-
-
-bc=np.bincount(values,minlength=512*480*8)
-
-#%timeit np.bincount(values)
-#%timeit np.histogram(values)
