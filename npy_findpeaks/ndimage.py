02d7b0064e13c6830e9d92dd46e3872106bcef3d
Ross Whitfield <whitfieldre@ornl.gov>
2018-04-04 18:19:09 -0400

    Update npy_findpeaks/ndimage.py

--- npy_findpeaks/ndimage.py
+++ npy_findpeaks/ndimage.py
@@ -8,10 +8,10 @@ threshold = 1e-8
 
 b=a[:,:,250]
 b[np.isnan(b)]=0
-b[threshold]=0
+b[b<threshold]=0
 
 
-plt.imshow(b,vmin=threshold)
+plt.imshow(b,vmin=threshold,vmax=threshold*2)
 plt.colorbar()
 plt.show()
 
@@ -25,11 +25,22 @@ for xxx, yyy in com:
     x.append(xxx)
     y.append(yyy)
 
+plt.scatter(y,-np.array(x))
+plt.show()
+
+
+plt.pcolormesh(b,vmin=threshold)
+plt.colorbar()
 plt.scatter(x, y)
 plt.show()
 
 
-plt.imshow(b,vmin=threshold)
-plt.scatter(x, y)
+c=a[:,:,250]
+c[np.isnan(c)]=0
+c=ndimage.grey_dilation(c,size=(3,3))
+threshold=2e-9
+c[c<threshold]=0
+
+plt.imshow(c,vmax=threshold)
 plt.colorbar()
 plt.show()
