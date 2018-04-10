ee0d6a710f54cf1ad731b4a2d0e149ed0373d1f3
Ross Whitfield <whitfieldre@ornl.gov>
2018-04-10 17:20:59 -0400

    Update IO_int/eventNexusToMDHisto_setSignal.py

---
+++ IO_int/eventNexusToMDHisto_setSignal.py
@@ -0,0 +1,35 @@
+from mantid.simpleapi import CreateMDHistoWorkspace
+import h5py
+import numpy as np
+import time
+runs=range(15954, 15954+1801) #17754+1)
+ipts=20367
+
+instrument='WAND'
+wavelength = 1.488
+
+pixels = 480*512*8
+npoints = len(runs)
+
+t0=time.time()
+data_array = np.empty((npoints, 512, 480*8), dtype=np.int64)
+phi_array = np.empty((npoints), dtype=np.float64)
+
+for n, run in enumerate(runs):
+    filename='/HFIR/HB2C/IPTS-{}/nexus/HB2C_{}.nxs.h5'.format(ipts,run)
+    print(filename)
+    with h5py.File(filename, 'r') as f_in:
+        bc = np.zeros((pixels),dtype=np.int64)
+        for b in range(8):
+            bc += np.bincount(f_in['/entry/bank'+str(b+1)+'_events/event_id'].value,minlength=pixels)
+        bc=bc.reshape((-1,512)).T
+        data_array[n] = bc
+        phi_array[n] = f_in['entry/DASlogs/HB2C:Mot:s1.RBV/average_value'].value[0]
+
+t1=time.time()
+print(t1-t0)
+print(data_array.shape)
+
+mdws = CreateMDWorkspace(Dimensions=3, Extents='-10,10,-10,10,-10,10', Names='A,B,C', Units='U,U,U')
+binned_ws = BinMD(InputWorkspace=mdws, AlignedDim0='A,0,10,1800', AlignedDim1='B,-10,10,3840', AlignedDim2='C,-10,10,512')
+binned_ws.setSignalArray(data_array)
