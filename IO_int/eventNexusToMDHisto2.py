89106b60981780fde0f85e43e3987061b81721ad
Ross Whitfield <whitfieldre@ornl.gov>
2018-04-10 23:19:08 -0400

    Update IO_int/eventNexusToMDHisto2.py

---
+++ IO_int/eventNexusToMDHisto2.py
@@ -0,0 +1,57 @@
+from mantid.simpleapi import CreateMDHistoWorkspace
+import mantid
+import h5py
+import numpy as np
+import time
+runs=range(15954, 15954+1801) #17754+1)
+ipts=20367
+
+instrument='WAND'
+wavelength = 1.488
+
+pixels = 480*512*8
+npoints = len(runs)
+
+t0=time.time()
+data_array = np.empty((npoints, 512, 480*8), dtype=np.int64)
+phi_array = np.empty((npoints), dtype=np.float64)
+
+for n, run in enumerate(runs):
+    filename='/HFIR/HB2C/IPTS-{}/nexus/HB2C_{}.nxs.h5'.format(ipts,run)
+    print(filename)
+    with h5py.File(filename, 'r') as f_in:
+        bc = np.zeros((pixels),dtype=np.int64)
+        for b in range(8):
+            bc += np.bincount(f_in['/entry/bank'+str(b+1)+'_events/event_id'].value,minlength=pixels)
+        bc=bc.reshape((-1,512)).T
+        data_array[n] = bc
+        phi_array[n] = f_in['entry/DASlogs/HB2C:Mot:s1.RBV/average_value'].value[0]
+
+t1=time.time()
+print(t1-t0)
+print(data_array.shape)
+"""
+md=CreateMDHistoWorkspace(SignalInput=data_array.flatten('F'),
+                          ErrorInput=data_array.flatten('F'),
+                          Dimensionality=3,
+                          Extents='{},{},0.5,512.5,0.5,3840.5'.format(phi_array[0],phi_array[-1]),
+                          NumberOfBins='{},{},{}'.format(npoints,512,3840),
+                          Names='phi,x,y',
+                          Units='degrees,bin,bin',
+                          EnableLogging=False)
+
+"""
+
+createWS_alg = mantid.api.PythonAlgorithm.createChildAlgorithm("CreateMDHistoWorkspace", enableLogging=False)
+createWS_alg.setProperty("SignalInput", data_array)
+createWS_alg.setProperty("ErrorInput", data_array)
+createWS_alg.setProperty("Dimensionality", 3)
+createWS_alg.setProperty("Extents", '{},{},0.5,512.5,0.5,3840.5'.format(phi_array[0],phi_array[-1]))
+createWS_alg.setProperty("NumberOfBins", '{},{},0.5,512.5,0.5,3840.5'.format(phi_array[0],phi_array[-1]))
+createWS_alg.setProperty("Names", 'phi,x,y')
+createWS_alg.setProperty("Units", 'a,b,c')
+createWS_alg.execute()
+
+t2=time.time()
+print(t2-t1)
+
