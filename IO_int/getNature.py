9429e4f385da1d55bc84222723e67db689b12030
Ross Whitfield <whitfieldre@ornl.gov>
2018-05-02 10:22:17 -0400

    Update IO_int/getNature.py autoreduce/reduce_HB2C.py

--- IO_int/getNature.py
+++ IO_int/getNature.py
@@ -1,6 +1,16 @@
+import h5py
 from mantid.simpleapi import LoadEventNexus
-ws = LoadEventNexus(Filename='/HFIR/HB2C/IPTS-7776/nexus/HB2C_26625.nxs.h5', MetaDataOnly=True)
+
+filename = '/HFIR/HB2C/IPTS-7776/nexus/HB2C_26625.nxs.h5'
+
+ws = LoadEventNexus(Filename=filename, MetaDataOnly=True)
 if ws.run().hasProperty('HB2C:CS:ITEMS:Nature'):
     nature = ws.run().getProperty('HB2C:CS:ITEMS:Nature').value[0]
     print(nature)
     print('Powder: {}'.format(nature == "Powder"))
+
+with h5py.File(filename, 'r') as f:
+    if '/entry/DASlogs/HB2C:CS:ITEMS:Nature' in f:
+        nature = f['/entry/DASlogs/HB2C:CS:ITEMS:Nature/value'].value[0][0]
+        print(nature)
+        print('Powder: {}'.format(nature == "Powder"))
--- autoreduce/reduce_HB2C.py
+++ autoreduce/reduce_HB2C.py
@@ -1,54 +1,33 @@
 #!/usr/bin/env python2
-import os
 import sys
+import h5py
+
+filename = sys.argv[1]
+outdir = sys.argv[2]
+
+with h5py.File(filename, 'r') as f:
+    if '/entry/DASlogs/HB2C:CS:ITEMS:Nature' in f:
+        nature = f['/entry/DASlogs/HB2C:CS:ITEMS:Nature/value'].value[0][0]
+        if nature != 'Powder':
+            print("Sample is {}, skipping autoreduction\nAutoreduction is only run on Powder samples".format(nature))
+            sys.exit()
+    else:
+        print("Sample Nature not found, skipping autoreduction")
+        sys.exit()
 
 sys.path.append("/opt/mantidnightly/bin")
-from mantid.simpleapi import *
-from mantid import logger
+from mantid.simpleapi import LoadWAND, WANDPowderReduction, SavePlot1D
 
-powder = False
+van_file = '/HFIR/HB2C/IPTS-7776/nexus/HB2C_26509.nxs.h5'
 
-if (len(sys.argv) != 3): 
-    logger.error("autoreduction code requires a filename and an output directory")
-    sys.exit()
-if not(os.path.isfile(sys.argv[1])):
-    logger.error("data file " + sys.argv[1] + " not found")
-    sys.exit()    
-else:
-    filename = sys.argv[1]
-    outdir = sys.argv[2]
-nexus_file = sys.argv[1]
-output_directory = sys.argv[2]
-output_file = os.path.split(nexus_file)[-1].replace('.nxs.h5','')
-
-ws = LoadWAND(filename)
-ConvertToMD('ws', QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_sample', OutputWorkspace='md',MinValues='-10,-1,-10',MaxValues='10,1,10')
-SaveMD('md',os.path.join(output_directory,output_file+"_MDE.nxs"))
-
-if powder:
-    SaveNexus('ws',os.path.join(output_directory,output_file+".nxs"))
-    runNumber = mtd['ws'].getRunNumber()
-    s2 = mtd['ws'].run().getLogData('HB2C:Mot:s2').firstValue()
-    ConvertSpectrumAxis(InputWorkspace='ws', Target='Theta', OutputWorkspace='ws')
-    Transpose(InputWorkspace='ws', OutputWorkspace='ws')
-    ResampleX(InputWorkspace='ws', OutputWorkspace='ws', XMin=s2, XMax=s2+120, NumberBins=2400)
-
-    if True: # do cal and monitor normalisation
-        van_file = '/HFIR/HB2C/IPTS-7776/shared/autoreduce/HB2C_7553.nxs'
-        cal = LoadNexus(Filename=van_file)
-        ws_monitor = mtd['ws'].run().getProtonCharge()
-        cal_monitor = cal.run().getProtonCharge()
-        CopyInstrumentParameters('ws', 'cal')
-        ConvertSpectrumAxis(InputWorkspace='cal', Target='Theta', OutputWorkspace='cal')
-        Transpose(InputWorkspace='cal', OutputWorkspace='cal')
-        ResampleX(InputWorkspace='cal', OutputWorkspace='cal', XMin=s2, XMax=s2+120, NumberBins=2400)
-        Divide(LHSWorkspace='ws', RHSWorkspace='cal', OutputWorkspace='ws')
-        Scale(InputWorkspace='ws', OutputWorkspace='ws', Factor=cal_monitor/ws_monitor)
-
-    div = SavePlot1D('ws', OutputType='plotly')
-    from postprocessing.publish_plot import publish_plot
-    #from finddata import publish_plot
-    request = publish_plot('HB2C', runNumber, files={'file': div})
-    print("post returned %d" % request.status_code)
-    print("resulting document:")
-    print(request.text)
+data = LoadWAND(filename)
+runNumber = data.getRunNumber()
+cal = LoadWAND(van_file)
+WANDPowderReduction(InputWorkspace=data,
+                    CalibrationWorkspace=cal,
+                    Target='Theta',
+                    NumberBins=1000,
+                    OutputWorkspace='reduced')
+div = SavePlot1D('reduced', OutputType='plotly')
+from postprocessing.publish_plot import publish_plot
+request = publish_plot('HB2C', runNumber, files={'file': div})
