5ee95e0dae258e00bc8e6632cb32670e42c2a39b
Ross Whitfield <whitfieldre@ornl.gov>
2018-05-09 16:19:14 -0400

    Update single2/create_grouping_file2.py

--- single2/create_grouping_file2.py
+++ single2/create_grouping_file2.py
@@ -15,3 +15,19 @@ for x in range(0,480*8,4):
                 grouping.dataY(y+i+(x+j)*512)[0] = group
 
 SaveDetectorsGrouping(grouping, "HB2C_4x4.xml")
+
+
+result = CreateGroupingWorkspace(InstrumentName = 'WAND')
+
+grouping = result[0]
+
+group = 0
+
+for x in range(0,480*8,2):
+    for y in range(0,512,2):
+        group += 1
+        for j in range(2):
+            for i in range(2):
+                grouping.dataY(y+i+(x+j)*512)[0] = group
+
+SaveDetectorsGrouping(grouping, "HB2C_2x2.xml")
