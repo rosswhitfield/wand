bb41f6950461c4beed7da45ff410bd0702170a1b
Ross Whitfield <whitfieldre@ornl.gov>
2018-05-09 14:19:13 -0400

    Update WANDSCD_test2.py

--- WANDSCD_test2.py
+++ WANDSCD_test2.py
@@ -16,3 +16,7 @@ LoadWANDSCD(IPTS=7776, RunNumbers=','.join(str(n) for n in range(26640,27944,5))
 ConvertWANDSCDtoQ(InputWorkspace='data', NormalisationWorkspace='norm',OutputWorkspace='q_norm',KeepTemporaryWorkspaces=True)
 ConvertWANDSCDtoQ(InputWorkspace='data',OutputWorkspace='q',KeepTemporaryWorkspaces=True)
 ConvertWANDSCDtoQ(InputWorkspace='data',OutputWorkspace='q2',KeepTemporaryWorkspaces=True,NormaliseBy='None')
+
+FindPeaksMD(InputWorkspace='q_norm', PeakDistanceThreshold=2, MaxPeaks=100, CalculateGoniometerForCW=True, Wavelength=1.488, OutputWorkspace='peaks')
+FindUBUsingLatticeParameters(PeaksWorkspace='peaks', a=5.64, b=5.64, c=5.64, alpha=90, beta=90, gamma=90)
+ConvertWANDSCDtoQ(InputWorkspace='data', NormalisationWorkspace='norm',UBWorkspace='peaks',Frame='HKL',OutputWorkspace='hkl_norm',BinningDim0='-0.6,0.6,61',BinningDim2='-6.51,6.51,651',BinningDim1='-2.01,7.01,451',KeepTemporaryWorkspaces=True)
