817e1e1c745ef7b5547a775bc299fdea1cfb96fe
Ross Whitfield <whitfieldre@ornl.gov>
2018-08-06 13:19:36 -0400

    Update autoreduce/.#create_vanadium_npy.py autoreduce/create_vanadium_npy.py autoreduce/reduce_HB2C.py

--- autoreduce/create_vanadium_npy.py
+++ autoreduce/create_vanadium_npy.py
@@ -1,11 +1,16 @@
-with h5py.File(filename, 'r') as f:
-        offset=f['/entry/DASlogs/HB2C:Mot:s2.RBV/average_value'].value[0]
-        title=f['/entry/title'].value[0]
-        bc = np.zeros((512*480*8),dtype=np.int64)
-        for b in range(8):
-                bc += np.bincount(f['/entry/bank'+str(b+1)+'_events/event_id'].value,minlength=512*480*8)
-        bc = bc.reshape((480*8, 512))
-        bc = (bc[::4,::4]    + bc[1::4,::4]  + bc[2::4,::4]  + bc[3::4,::4]
-              + bc[::4,1::4] + bc[1::4,1::4] + bc[2::4,1::4] + bc[3::4,1::4]
-              + bc[::4,2::4] + bc[1::4,2::4] + bc[2::4,2::4] + bc[3::4,2::4]
-              + bc[::4,3::4] + bc[1::4,3::4] + bc[2::4,3::4] + bc[3::4,3::4])
+import numpy as np
+import h5py
+
+runNumber = 29780
+
+with h5py.File('/HFIR/HB2C/IPTS-7776/nexus/HB2C_{}.nxs.h5'.format(runNumber), 'r') as f:
+    bc = np.zeros((512*480*8))
+    for b in range(8):
+        bc += np.bincount(f['/entry/bank'+str(b+1)+'_events/event_id'].value,minlength=512*480*8)
+    bc = bc.reshape((480*8, 512))
+    bc = (bc[::4,::4]    + bc[1::4,::4]  + bc[2::4,::4]  + bc[3::4,::4]
+          + bc[::4,1::4] + bc[1::4,1::4] + bc[2::4,1::4] + bc[3::4,1::4]
+          + bc[::4,2::4] + bc[1::4,2::4] + bc[2::4,2::4] + bc[3::4,2::4]
+          + bc[::4,3::4] + bc[1::4,3::4] + bc[2::4,3::4] + bc[3::4,3::4])
+
+np.save('vanadium_{}.npy'.format(runNumber), bc)
--- autoreduce/reduce_HB2C.py
+++ autoreduce/reduce_HB2C.py
@@ -42,7 +42,8 @@ else: # Single Crystal
     with h5py.File(filename, 'r') as f:
         offset=f['/entry/DASlogs/HB2C:Mot:s2.RBV/average_value'].value[0]
         title=f['/entry/title'].value[0]
-        bc = np.zeros((512*480*8),dtype=np.int64)
+        mon=f['/entry/monitor1/total_counts'].value[0]
+        bc = np.zeros((512*480*8))
         for b in range(8):
             bc += np.bincount(f['/entry/bank'+str(b+1)+'_events/event_id'].value,minlength=512*480*8)
         bc = bc.reshape((480*8, 512))
@@ -50,16 +51,21 @@ else: # Single Crystal
               + bc[::4,1::4] + bc[1::4,1::4] + bc[2::4,1::4] + bc[3::4,1::4]
               + bc[::4,2::4] + bc[1::4,2::4] + bc[2::4,2::4] + bc[3::4,2::4]
               + bc[::4,3::4] + bc[1::4,3::4] + bc[2::4,3::4] + bc[3::4,3::4])
+
+    vanadium = np.load('/HFIR/HB2C/shared/autoreduce/vanadium.npy')
+    vanadium_mon = 103054259
+    bc = bc / vanadium * vanadium_mon / mon
     f, (ax1, ax2) = plt.subplots(2, figsize=(8,4))
     ax1.set_title(u'{}, {}, s2={:.2f}'.format(title,output_file,offset))
     ax1.plot(np.linspace(offset,120+offset,960),bc.sum(1)[::-1])
     ax1.set_xlim(offset,120+offset)
     plt.setp(ax1.get_xticklabels(), visible=False)
-    ax2.imshow(bc.T[::-1,::-1], cmap='viridis',aspect=1/7.5,extent=(offset,120+offset,0,128),norm=LogNorm(vmin=1, vmax=bc.max()))
+    ax2.imshow(bc.T[::-1,::-1], cmap='viridis',aspect=1/7.5,extent=(offset,120+offset,0,128))
     ax2.set_xlabel(u'2theta')
     ax2.set_xlim(offset,120+offset)
     ax2.set_ylim(0,128)
     ax2.get_yaxis().set_visible(False)
     f.tight_layout()
-    f.subplots_adjust(hspace=0.001)
-    plt.savefig(outdir+output_file)
+    plt.subplot_tool()
+    plt.show()
+    #plt.savefig(outdir+output_file)
