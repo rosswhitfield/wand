9f68a28424f90ce1b35f21380750885b99da86d8
Ross Whitfield <whitfieldre@ornl.gov>
2018-08-17 15:19:11 -0400

    Update Pr2NiO4_new.py

--- Pr2NiO4_new.py
+++ Pr2NiO4_new.py
@@ -1,12 +1,18 @@
+import numpy as np
+
 LoadWANDSCD(IPTS=7776, RunNumbers='4756-6557', Grouping='4x4', OutputWorkspace='data')
 ConvertWANDSCDtoQ(InputWorkspace='data', NormaliseBy='None', KeepTemporaryWorkspaces=True, OutputWorkspace='q', BinningDim0='-8.01,8.01,801', BinningDim1='-0.81,0.81,81',BinningDim2='-8.01,8.01,801')
 
 FindPeaksMD(InputWorkspace='q_data', PeakDistanceThreshold=1, MaxPeaks=100, DensityThresholdFactor=500, CalculateGoniometerForCW=True, Wavelength=1.488, OutputWorkspace='peaks')
 
+tt0=np.array(data.getExperimentInfo(0).run().getLogData('twotheta').value)
+az0=np.array(data.getExperimentInfo(0).run().getLogData('azimuthal').value)
+
+
 FindUBUsingLatticeParameters('peaks',5.414,5.414,12.457,90,90,90)
 
 ws=LoadEventNexus(Filename='/HFIR/HB2C/IPTS-7776/nexus/HB2C_4756.nxs.h5', MetaDataOnly=True)
-AddSampleLog(ws, LogName='HB2C:Mot:detz.RBV', LogText='3.4', LogType='Number Series')
+AddSampleLog(ws, LogName='HB2C:Mot:detz.RBV', LogText='3.85', LogType='Number Series')
 LoadInstrument(ws, InstrumentName='WAND', RewriteSpectraMap='True')
 
 grouping = 4
