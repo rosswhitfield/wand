ea0e4f828210d8a89ff639e204f960477ffbaf0e
Ross Whitfield <whitfieldre@ornl.gov>
2018-08-17 11:22:02 -0400

    Update Pr2NiO4_new.py

---
+++ Pr2NiO4_new.py
@@ -0,0 +1,2 @@
+LoadWANDSCD(IPTS=7776, RunNumbers='4756-6557', Grouping='4x4', OutputWorkspace='data')
+ConvertWANDSCDtoQ(InputWorkspace='data', NormaliseBy='None', KeepTemporaryWorkspaces=True, OutputWorkspace='q', BinningDim0='-8.01,8.01,801', BinningDim1='-0.81,0.81,81',BinningDim2='-8.01,8.01,801')
