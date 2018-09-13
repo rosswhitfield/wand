e2e2354cb3c6636216de24c2d071b86052f9532d
Ross Whitfield <whitfieldre@ornl.gov>
2018-09-13 12:19:06 -0400

    Update lipid_blayer.py

--- lipid_blayer.py
+++ lipid_blayer.py
@@ -1,7 +1,7 @@
 LoadWANDSCD(IPTS='17751', RunNumbers='64974-65134', OutputWorkspace='DPPC')
 LoadWANDSCD(IPTS='7776', RunNumbers='124736', OutputWorkspace='vanadium')
 
-ReplicateMD(ShapeWorkspace='DPPC', DataWorkspace='vanadium', OutputWorkspace='vanadium_rep')
-DivideMD(LHSWorkspace='DPPC', RHSWorkspace='vanadium_rep', OutputWorkspace='DPPC_normed')
+ReplicateMD(ShapeWorkspace='DPPC', DataWorkspace='vanadium', OutputWorkspace='vanadium')
+DivideMD(LHSWorkspace='DPPC', RHSWorkspace='vanadium', OutputWorkspace='DPPC')
 
-IntegrateMDHistoWorkspace(InputWorkspace='DPPC_normed', P1Bin='160,350', OutputWorkspace='DPPC_int')
+IntegrateMDHistoWorkspace(InputWorkspace='DPPC', P1Bin='160,350', OutputWorkspace='DPPC_int')
