bfa9b091a6817e36015de0d99f273ac044c3835b
Ross Whitfield <whitfieldre@ornl.gov>
2018-09-13 10:25:41 -0400

    Update .#lipid_blayer.py lipid_blayer.py

---
+++ lipid_blayer.py
@@ -0,0 +1,3 @@
+LoadWANDSCD(IPTS='17751', RunNumbers='64974-65134', OutputWorkspace='DPPC')
+LoadWANDSCD(IPTS='7776', RunNumbers='124736', OutputWorkspace='vanadium')
+IntegrateMDHistoWorkspace(InputWorkspace='DPPC', P1Bin='160,350', OutputWorkspace='DPPC_int')
