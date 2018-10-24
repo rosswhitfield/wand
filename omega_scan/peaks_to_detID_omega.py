62836df488f8523e1cf46fb93e26c4de3a4553c8
Ross Whitfield <whitfieldre@ornl.gov>
2018-10-24 11:19:14 -0400

    Update omega_scan/peaks_to_detID_omega.py

--- omega_scan/peaks_to_detID_omega.py
+++ omega_scan/peaks_to_detID_omega.py
@@ -1,7 +1,9 @@
 from mantid.simpleapi import LoadNexus
+import numpy as np
 
 peaks = LoadNexus('peaks.nxs')
 
 for p in range(peaks.getNumberPeaks()):
     peak = peaks.getPeak(p)
-    
+    g = peak.getGoniometerMatrix()
+    print(peak.getDetectorID(), peak.getQSampleFrame(), np.mod(np.arctan(g[0,2]/g[0,0])*180/np.pi,-180))
