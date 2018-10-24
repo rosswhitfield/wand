77d60a94d8ac4a96032266078d9da61db98ea01d
Ross Whitfield <whitfieldre@ornl.gov>
2018-10-24 13:19:12 -0400

    Update omega_scan/.#peaks_to_detID_omega.py omega_scan/peaks_to_detID_omega.py

--- omega_scan/peaks_to_detID_omega.py
+++ omega_scan/peaks_to_detID_omega.py
@@ -6,4 +6,7 @@ peaks = LoadNexus('peaks.nxs')
 for p in range(peaks.getNumberPeaks()):
     peak = peaks.getPeak(p)
     g = peak.getGoniometerMatrix()
-    print(peak.getDetectorID(), peak.getQSampleFrame(), np.mod(np.arctan(g[0,2]/g[0,0])*180/np.pi,-180))
+    detID = peak.getDetectorID()
+    x=detID//(4*4*128)
+    y=detID%(4*128)//4
+    print(detID, peak.getQSampleFrame(), np.mod(np.arctan(g[0,2]/g[0,0])*180/np.pi,-180), x, y)
