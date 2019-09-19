4d95169aa5424678c180f7e72140ba4480028b0c
Ross Whitfield <whitfieldre@ornl.gov>
2019-09-19 12:19:21 -0400

    Update intergartion_test_MDE.py

---
+++ intergartion_test_MDE.py
@@ -0,0 +1,12 @@
+# The following line helps with future compatibility with Python 3
+# print must now be used as a function, e.g print('Hello','World')
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+
+# import mantid algorithms, numpy and matplotlib
+from mantid.simpleapi import *
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+data = LoadWANDSCD(IPTS=21362, RunNumbers='120754-122554', Grouping='4x4')
