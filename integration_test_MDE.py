e410671e3788fc6d831d9d3811dea4e89fd42d12
Ross Whitfield <whitfieldre@ornl.gov>
2019-11-22 09:19:15 -0500

    Update integration_test_MDE.py

--- integration_test_MDE.py
+++ integration_test_MDE.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 data = LoadWANDSCD(IPTS=21362, RunNumbers='120754-122554', Grouping='4x4')
-norm=LoadWANDSCD("HB2C137518", Grouping='4x4')
+norm=LoadWANDSCD(IPTS=7776,RunNumbers='137518', Grouping='4x4')
 mde=ConvertWANDSCDtoMDE(data)
 ConvertWANDSCDtoQ(InputWorkspace='data',
                   NormalisationWorkspace='norm',
