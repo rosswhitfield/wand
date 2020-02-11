8f36aaab7ae7a53552eaa95ae78bfee38c952d56
Ross Whitfield <whitfieldre@ornl.gov>
2020-02-11 11:19:06 -0500

    Update Al2O3_integration_test_MDE.py

--- Al2O3_integration_test_MDE.py
+++ Al2O3_integration_test_MDE.py
@@ -10,20 +10,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 data = LoadWANDSCD(IPTS=22745, RunNumbers='147131-148931', Grouping='4x4')
-mde=ConvertSCDtoMDE(data, wavelength=1.488,MinValues='-10,-10,-10',MaxValues='10,10,10')
+mde = ConvertHFIRSCDtoMDE(data, wavelength=1.488,MinValues='-10,-10,-10',MaxValues='10,10,10')
 
 PredictPeaks(InputWorkspace=data,
-MinDSpacing=0.5,
-ReflectionCondition='Rhombohedrally centred, obverse',
+             MinDSpacing=0.5,
+             ReflectionCondition='Rhombohedrally centred, obverse',
              CalculateGoniometerForCW=True,
-             Wavelength=1.488, MinAngle=-90,MaxAngle=90,
+             Wavelength=1.488,
+             MinAngle=-90,MaxAngle=90,
              CalculateStructureFactors=True,
              OutputWorkspace='predict')
 
-IntegratePeaksMD(InputWorkspace=mde, PeaksWorkspace='predict', PeakRadius=0.5, OutputWorkspace='integrated')
+IntegratePeaksMD(InputWorkspace=mde,
+                 PeaksWorkspace='predict',
+                 PeakRadius=0.5,
+                 OutputWorkspace='integrated')
 
 peaks = mtd["integrated"]
 for p in range(peaks.getNumberPeaks()):
         peak = peaks.getPeak(p)
-        lorentz = np.sin(2*np.arcsin(1.488/(2*peak.getDSpacing())))
-        print("HKL = {:>12} int = {:>8} {:5.1f}".format(str(peak.getHKL()),peak.getIntensity(),peak.getIntensity()*lorentz))
+        print("HKL = {:>12} int = {:>8} {:5.1f}".format(str(peak.getHKL()),peak.getIntensity(),peak.getIntensity()))
