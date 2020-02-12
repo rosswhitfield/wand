1cbde8f36d2ce3e6067a2d7bf253ff8ba4e5bdbc
Ross Whitfield <whitfieldre@ornl.gov>
2020-02-12 17:19:34 -0500

    Update Al2O3.py

--- Al2O3.py
+++ Al2O3.py
@@ -18,22 +18,17 @@ PredictPeaks(InputWorkspace=data,
              CalculateGoniometerForCW=True,
              Wavelength=1.488,
              MinAngle=-90,MaxAngle=90,
-             CalculateStructureFactors=True,
              OutputWorkspace='predict')
 
 IntegratePeaksMD(InputWorkspace=mde,
                  PeaksWorkspace='predict',
                  PeakRadius=0.5,
                  OutputWorkspace='integrated')
+SaveReflections('integrated', Filename='/SNS/users/rwp/wand/Al2O3_integrated_peaks.hkl')
 
 IntegratePeaksMD(InputWorkspace=mde,
                  PeaksWorkspace='predict',
                  PeakRadius=0.5,
                  BackgroundOuterRadius=0.75,
                  OutputWorkspace='integrated_bkg')
-
-peaks = mtd['integrated']
-for p in range(peaks.getNumberPeaks()):
-        peak = peaks.getPeak(p)
-        if peak.getL() == 0:
-            print("HKL = {:>12} int = {:>8}".format(str(peak.getHKL()),peak.getIntensity()))
+SaveReflections('integrated_bkg', Filename='/SNS/users/rwp/wand/Al2O3_integrated_peaks_bkg_subtract.hkl')
